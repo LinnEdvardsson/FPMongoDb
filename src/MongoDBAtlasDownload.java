d77134dd383a98f28d644804c250247016943b8c
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2025-02-14 13:34:42 +0100

    new output methods

--- src/MongoDBAtlasDownload.java
+++ src/MongoDBAtlasDownload.java
@@ -11,6 +11,8 @@ import static java.util.Locale.forLanguageTag;
 //"mongodb+srv://linnedvardsson:bulle123@cluster0.kh0oq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 public class MongoDBAtlasDownload {
 
+    List<Movie> movieList;
+
     public MongoDBAtlasDownload() {
 
         //Skriv in rätt uri!
@@ -29,18 +31,12 @@ public class MongoDBAtlasDownload {
                 }
             }
 
+//             Skriver ut alla filmer
+            for (Movie movie : movieList) {
+                System.out.println(movie);
+            }
 
-            // Skriver ut alla filmer
-//            for (Movie movie : movieList) {
-//                System.out.println(movie);
-//            }
-
-            //Här gör du anrop till alla dina funktioner som ska skriva ut svaren på frågorna som
-            //efterfrågas i uppgiften
-
-//            System.out.println(getNumbOfMovies(movieList));
-            getAllQuestions(movieList);
-
+            getAllQuestions();
 
 
         } catch (Exception e) {
@@ -48,17 +44,17 @@ public class MongoDBAtlasDownload {
         }
     }
 
-    public void getAllQuestions(List<Movie> movieList) {
-        System.out.println("Number of movies from 1975: " + getNumbOfMovies(movieList));
-        System.out.println("Longest movie was: " + getLongestMovie(movieList) + " minutes");
-        System.out.println("Unique genres: " + getUniqueGenres(movieList));
-        System.out.println("Actors in highest rated movie is: " + getActors(movieList));
-        System.out.println("Least number of actors in movie: " + getLeastActors(movieList));
-        System.out.println("Number of actors in more than 1 movie: " + actorsInMovies(movieList));
-        System.out.println("Pop actor: " + getMostPopularActor(movieList));
-        System.out.println("Number of unique languages: " + getUniquelanguage(movieList));
-        System.out.println("Movie title belongs to several movies: " + getMulitMovieTitle(movieList));
-    }
+      public void getAllQuestions() {
+          NumbOfMovies();
+          LongestMovie();
+          UniqueGenres();
+          HighestRatedMovieActors();
+          MovieWithLeastActors();
+          ActorsInMultipleMovies();
+          PopularActors();
+          Uniquelanguage();
+          MultiMovieTitle();
+      }
 
     // Hur många filmer gjordes 1975 (enligt vårt data). Returnera ett tal
     // Filtrerar filmer i listan på filmer från 1975, omvandlar varje match till 1 för att summera alla.
@@ -78,6 +74,7 @@ public class MongoDBAtlasDownload {
     }
 
     //Vilka skådisar som spelade i den film som hade högst imdb-rating. Returnera en List<String> med deras namn.(KAN VARA FLERA)
+
     /// Lägger in rating i en comparing som jämför objekten Movie baserat på rating. Sätter den i max för att returnera högsta värdet med skådespelare.
     public List<String> getActors(List<Movie> movieList) {
         Comparator<Movie> movieComparator = Comparator.comparing(Movie::getImdbRating);
@@ -92,13 +89,15 @@ public class MongoDBAtlasDownload {
     }
 
     //Hur många skådisar var med i mer än 1 film? Returnera ett tal.
+
     /// lägger resultat i hashmap för att ge nyckel (actor) och värde (förekommer antal ggr). Går igenom listan och filtrerar på värden och summerar hur många gånger man förekommer.
     public int actorsInMovies(List<Movie> movieList) {
         Map<String, Long> acc = movieList.stream().flatMap(m -> m.getCast().stream()).collect(Collectors.groupingBy(x -> x, Collectors.counting()));
         return acc.entrySet().stream().filter(e -> e.getValue() > 1).mapToInt(e -> 1).sum();
     }
 
-    //Vad hette den skådis som var med i flest filmer? Returnera en String ("Gene Hackman")
+    //Vad hette den skådis som var med i flest filmer? Returnera en String
+
     /// samma comparing som ovan, men i return nu jämför values i mappen, och det högsta values hämtas nyckeln för.
     public String getMostPopularActor(List<Movie> movieList) {
         Map<String, Long> pop = movieList.stream().flatMap(m -> m.getCast().stream()).collect(Collectors.groupingBy(x -> x, Collectors.counting()));
@@ -114,7 +113,41 @@ public class MongoDBAtlasDownload {
         return movieTitles.entrySet().stream().anyMatch(e -> e.getValue() > 1);
     }
 
+    public void NumbOfMovies() {
+        System.out.println("Number of movies from 1975: " + getNumbOfMovies(movieList));
+    }
 
+    public void LongestMovie() {
+        System.out.println("Longest movie was: " + getLongestMovie(movieList) + " minutes");
+    }
+
+    public void UniqueGenres() {
+        System.out.println("Unique genres: " + getUniqueGenres(movieList));
+    }
+
+    public void HighestRatedMovieActors() {
+        System.out.println("Actors in highest rated movie is: " + getActors(movieList));
+    }
+
+    public void MovieWithLeastActors() {
+        System.out.println("Least number of actors in movie: " + getLeastActors(movieList));
+    }
+
+    public void ActorsInMultipleMovies() {
+        System.out.println("Number of actors in more than 1 movie: " + actorsInMovies(movieList));
+    }
+
+    public void PopularActors() {
+        System.out.println("Pop actor: " + getMostPopularActor(movieList));
+    }
+
+    public void Uniquelanguage() {
+        System.out.println("Number of unique languages: " + getUniquelanguage(movieList));
+    }
+
+    public void MultiMovieTitle() {
+        System.out.println("Movie title belongs to several movies: " + getMulitMovieTitle(movieList));
+    }
 
     public static void main(String[] args) {
         MongoDBAtlasDownload m = new MongoDBAtlasDownload();
