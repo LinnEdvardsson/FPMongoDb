bcfdd23d47a9f4d92bb5d2a7cff09d6cbb3a8749
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2025-02-20 15:14:33 +0100

    final

--- src/Functions.java
+++ src/Functions.java
@@ -6,12 +6,10 @@ import java.util.stream.Stream;
 
 public class Functions {
 
-
-    Function<List<Movie>, Map<String, Long>> actorsInMultipleMovies = list -> list.stream().map(Movie::getCast).
-            flatMap(List::stream)
+    Function<List<Movie>, Map<String, Long>> actorsInMultipleMovies = list -> list.stream().map(Movie::getCast).flatMap(List::stream)
             .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
 
-    Function<List<Movie>, Stream<String>> uniqueLanguages= movieList -> movieList.stream().map(Movie::getLanguages).flatMap(List::stream);
+    Function<List<Movie>, Stream<String>> uniqueLanguages = movieList -> movieList.stream().map(Movie::getLanguages).flatMap(List::stream);
 
     Function<List<Movie>, Map<String, Long>> moviesWithDuplicateTitles = movieList -> movieList.stream().map(Movie::getTitle)
             .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
@@ -21,5 +19,4 @@ public class Functions {
     Function<List<Movie>, Map<String, Long>> mostPopularActor = list -> list.stream().map(Movie::getCast)
             .flatMap(List::stream)
             .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
-
 }
